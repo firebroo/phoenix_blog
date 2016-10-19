cc3677caf551bf98daf1391eebb431dabbeac009
firebroo <1121927992@qq.com>
2016-10-19 09:31:53 +0000

    修复session控制未登陆conn渲染两次

--- web/plugs/session_plug.ex
+++ web/plugs/session_plug.ex
@@ -1,18 +1,14 @@
 defmodule HelloPhoenix.Plugs.Session do
 
-  import Plug.Conn
-  import Phoenix.Controller, [:redirect]
-
-  alias HelloPhoenix.Router.Helpers
+  import Plug.Conn, only: [get_session: 2, halt: 1]
+  import Phoenix.Controller, only: [redirect: 2]
 
   def init(default), do: default
 
   def call(conn, _params) do
     if get_session(conn, :user_id) == nil do
-      conn 
-      |> redirect(to: Helpers.session_path(conn, :new))
+      conn |> redirect(to: "/users/login") |> halt
     end
-    conn
   end
 
 end

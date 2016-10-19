d6283df7afb28077de78eb6b1469dace474f563c
firebroo <1121927992@qq.com>
2016-10-19 09:14:28 +0000

    添加后台管理评论

--- web/controllers/user/comment_controller.ex
+++ web/controllers/user/comment_controller.ex
@@ -1,22 +1,68 @@
-defmodule HelloPhoenix.CommentController do
-    use HelloPhoenix.Web, :controller
-    
-    alias HelloPhoenix.{Comment}
+defmodule HelloPhoenix.User.CommentController do
+  use HelloPhoenix.Web, :controller
 
-    def create(conn, %{"category_id" => category_id, "article_id" => article_id, "comment" => comment_params}) do
-        comment_params = comment_params
-        |> Map.put("article_id", article_id)
+  alias HelloPhoenix.Comment
 
-        changeset = Comment.changeset(%Comment{}, comment_params)
-        case Repo.insert(changeset) do
-             {:ok, _comment} ->
-                conn
-                |> put_flash(:info, "评论提交成功.")
-             {:error, _changeset} ->
-                conn
-                |> put_flash(:error, "评论提交失败")
-        end
-        |> redirect(to: category_article_path(conn, :show, category_id, article_id))
+  def index(conn, _params) do
+    comment = Comment
+    |> Repo.all
+    |> Repo.preload(:article)
+
+    render(conn, "index.html", comment: comment)
+  end
+
+  def new(conn, _params) do
+    changeset = Comment.changeset(%Comment{})
+    render(conn, "new.html", changeset: changeset)
+  end
+
+  def create(conn, %{"comment" => comment_params}) do
+    changeset = Comment.changeset(%Comment{}, comment_params)
+
+    case Repo.insert(changeset) do
+      {:ok, _comment} ->
+        conn
+        |> put_flash(:info, "Comment created successfully.")
+        |> redirect(to: user_comment_path(conn, :index))
+      {:error, changeset} ->
+        render(conn, "new.html", changeset: changeset)
     end
+  end
 
+  def show(conn, %{"id" => id}) do
+    comment = Repo.get!(Comment, id)
+    render(conn, "show.html", comment: comment)
+  end
+
+  def edit(conn, %{"id" => id}) do
+    comment = Repo.get!(Comment, id)
+    changeset = Comment.changeset(comment)
+    render(conn, "edit.html", comment: comment, changeset: changeset)
+  end
+
+  def update(conn, %{"id" => id, "comment" => comment_params}) do
+    comment = Repo.get!(Comment, id)
+    changeset = Comment.changeset(comment, comment_params)
+
+    case Repo.update(changeset) do
+      {:ok, comment} ->
+        conn
+        |> put_flash(:info, "Comment updated successfully.")
+        |> redirect(to: user_comment_path(conn, :show, comment))
+      {:error, changeset} ->
+        render(conn, "edit.html", comment: comment, changeset: changeset)
+    end
+  end
+
+  def delete(conn, %{"id" => id}) do
+    comment = Repo.get!(Comment, id)
+
+    # Here we use delete! (with a bang) because we expect
+    # it to always work (and if it does not, it will raise).
+    Repo.delete!(comment)
+
+    conn
+    |> put_flash(:info, "Comment deleted successfully.")
+    |> redirect(to: user_comment_path(conn, :index))
+  end
 end

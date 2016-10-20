5d662497f89b01edd4cc4444945533e559566ef3
firebroo <1121927992@qq.com>
2016-10-20 05:23:30 +0000

    修改if...do为case...do

--- web/plugs/session_plug.ex
+++ web/plugs/session_plug.ex
@@ -2,14 +2,16 @@ defmodule HelloPhoenix.Plugs.Session do
 
   import Plug.Conn, only: [get_session: 2, halt: 1]
   import Phoenix.Controller, only: [redirect: 2]
+  alias HelloPhoenix.Router.Helpers
 
   def init(default), do: default
 
   def call(conn, _params) do
-    if get_session(conn, :user_id) == nil do
-      conn |> redirect(to: "/users/login") |> halt
-    else
-      conn
+    case get_session(conn, :user_id) do
+      nil ->
+        conn |> redirect(to: Helpers.session_path(conn, :new)) |> halt
+      _  -> 
+        conn
     end
   end
 

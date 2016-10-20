00eeaf89b711e7e392dc97e49326e4993d26a396
firebroo <1121927992@qq.com>
2016-10-20 01:40:41 +0000

    添加注册密码确认密码字段

--- web/controllers/user_controller.ex
+++ web/controllers/user_controller.ex
@@ -4,17 +4,13 @@ defmodule HelloPhoenix.UserController do
   alias HelloPhoenix.{User}
   alias HelloPhoenix.Router.Helpers
 
-  import Ecto.Changeset, only: [put_change: 3]
-  import Comeonin.Bcrypt, only: [hashpwsalt: 1]
-
   def new(conn, _params) do
     changeset = User.changeset(%User{})
     render conn, "new.html", changeset: changeset
   end
 
   def create(conn, %{"user" => user_params}) do
-     changeset = User.changeset(%User{}, user_params)
-     |> put_change(:password, hashpwsalt(user_params["password"])) 
+     changeset = User.changeset_register(%User{}, user_params)
 
      case Repo.insert(changeset) do
         {:ok, _user} ->
--- web/models/user.ex
+++ web/models/user.ex
@@ -1,10 +1,14 @@
 defmodule HelloPhoenix.User do
   use HelloPhoenix.Web, :model
 
+  alias Ecto.Changeset
+  import Comeonin.Bcrypt, only: [hashpwsalt: 1]
+
   schema "users" do
     field :name, :string
     field :password, :string
 
+    field :password_confirmation, :string, virtual: true
     timestamps()
   end
 
@@ -17,4 +21,25 @@ defmodule HelloPhoenix.User do
     |> validate_required([:name, :password])
     |> unique_constraint(:name)
   end
+
+  def put_password_hash(%Ecto.Changeset{valid?: true} = changeset, field) do
+    value = get_field(changeset, field)
+    
+    changeset |> put_change(:password, Comeonin.Bcrypt.hashpwsalt(value))
+  end
+      
+  def put_password_hash(%Ecto.Changeset{valid?: false} = changeset, _field) do
+     changeset
+  end
+
+  def changeset_register(struct, params \\ %{}) do
+    struct
+    |> cast(params, [:name, :password, :password_confirmation])
+    |> validate_required([:name, :password, :password_confirmation], message: "字段不能为空")
+    |> validate_length(:password, min: 6, message: "密码长度不能小于6位")
+    |> validate_confirmation(:password, message: "两次密码输入不一致")
+    |> unique_constraint(:name, message: "用户名已经被注册")
+    |> put_password_hash(:password)
+  end
+
 end

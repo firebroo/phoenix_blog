6647f7b9e0030118ec496273bebcb895dd06c1f2
firebroo <1121927992@qq.com>
2016-10-20 21:56:24 +0800

    添加不允许上传后缀

--- web/controllers/session_controller.ex
+++ web/controllers/session_controller.ex
@@ -2,6 +2,8 @@ defmodule HelloPhoenix.SessionController do
   use HelloPhoenix.Web, :controller
 
   alias HelloPhoenix.{User}
+  @allow_file_suffix [".gif", ".jpg", ".jpeg", ".png", ".svg"]
+  import Ecto.Changeset, only: [add_error: 4]
   
   def new(conn, _params) do
     changeset = User.changeset(%User{})
@@ -55,28 +57,34 @@ defmodule HelloPhoenix.SessionController do
   end
 
   def update_avatar(conn, %{"user" => user_params}) do
-    changeset = User.changeset(%User{})
     user_id = get_session(conn, :user_id)
     if upload = user_params["avatar"] do
       extension = Path.extname(upload.filename)
       path = "priv/static/images/#{user_id}-profile#{extension}"
+      
+      changeset = User.changeset_avatar(%User{})
+      case extension in @allow_file_suffix do
+        true ->
+          case File.cp(upload.path, path) do
+            :ok ->  # 上传成功, 将图片地址入库
+              User
+              |> Repo.get!(user_id)
+              |> User.changeset_avatar(%{"avatar" => "/images/#{user_id}-profile#{extension}"})
+              |> Repo.update
 
-      case File.cp(upload.path, path) do
-        :ok ->  # 上传成功, 将图片地址入库
-          User
-          |> Repo.get!(user_id)
-          |> User.changeset_avatar(%{"avatar" => "/images/#{user_id}-profile#{extension}"})
-          |> Repo.update
-
-          conn
-          |> put_flash(:info, "需改头像成功")
-          |> redirect(to: user_session_path(conn, :home))
-        {:error, _} ->
-          conn
-          |> assign(:changeset, changeset)
-          |> put_flash(:error, "需改头像失败")
-          |> render("photo_form.html")
+              conn
+              |> put_flash(:info, "修改头像成功")
+              |> redirect(to: user_session_path(conn, :home))
+            {:error, _} ->
+              changeset = changeset |> add_error(:avatar, "上传出错", message: "上传出错")
+          end
+        false ->
+          changeset = changeset |> add_error(:avatar, "不允许的后缀", message: "不允许的后缀")
       end
+      conn
+      |> assign(:changeset, changeset)
+      |> put_flash(:error, "修改头像失败")
+      |> render("photo_form.html")
     end
   end
 

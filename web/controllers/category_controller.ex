3aa1f8dc98c69afc24d8d4d29a631d3dd8d4b1f2
firebroo <1121927992@qq.com>
2016-11-04 09:38:33 +0000

    1. 重构首页显示内容 2. 文章内容显示修改 ‘

--- web/controllers/category_controller.ex
+++ web/controllers/category_controller.ex
@@ -1,11 +1,18 @@
 defmodule HelloPhoenix.CategoryController do
   use HelloPhoenix.Web, :controller
 
-  alias HelloPhoenix.Category
+  alias HelloPhoenix.{Category, Article}
 
   def index(conn, _params) do
     categorys = Repo.all(Category)
-    render(conn, "index.html", categorys: categorys)
+    articles = Article
+    |> Repo.all
+    |> Repo.preload([:category, :tags])
+
+    conn
+    |> assign(:categorys, categorys)
+    |> assign(:articles, articles)
+    |> render(:index)
   end
 
   def show(conn, %{"id" => id}) do
--- web/views/article_view.ex
+++ web/views/article_view.ex
@@ -1,3 +1,9 @@
 defmodule HelloPhoenix.ArticleView do
   use HelloPhoenix.Web, :view
+
+  def markdown(body) do
+    body
+    |> Earmark.to_html
+    |> raw
+  end
 end

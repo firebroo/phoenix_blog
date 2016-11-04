1b7d171e9c6cc51b962a7f159eae7d0a7f57224e
firebroo <1121927992@qq.com>
2016-11-04 09:36:34 +0000

    增加post 文章内容escape

--- web/models/article.ex
+++ web/models/article.ex
@@ -20,7 +20,21 @@ defmodule HelloPhoenix.Article do
     struct
     |> cast(params, [:title, :body, :reading, :category_id])
     |> validate_required([:title, :body, :category_id])
+    #|> strip_unsafe_body(params)
     |> validate_length(:title, min: 5)
     |> unique_constraint(:title)
+  end
+
+  defp strip_unsafe_body(model, %{"body" => nil}) do
+    model
+  end
+    
+  defp strip_unsafe_body(model, %{"body" => body}) do
+    {:safe, clean_body} = Phoenix.HTML.html_escape(body)
+    model |> put_change(:body, clean_body)
+  end
+    
+  defp strip_unsafe_body(model, _) do
+    model
   end
 end

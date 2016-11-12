3cef796961da588435e65ee1b112d40520f3be9d
firebroo <1121927992@qq.com>
2016-11-12 23:54:13 +0800

    添加404和500页面

--- web/views/error_view.ex
+++ web/views/error_view.ex
@@ -1,14 +1,13 @@
 defmodule HelloPhoenix.ErrorView do
   use HelloPhoenix.Web, :view
 
-  def render("404.html", _assigns) do
-    "Page not found"
+  def render("404.html", assigns) do
+    render "not_found.html", assigns
   end
 
-  def render("500.html", _assigns) do
-    "Internal server error"
+  def render("500.html", assigns) do
+    render "internal_error.html", assigns
   end
-
   # In case no render clause matches or no
   # template is found, let's render it as 500
   def template_not_found(_template, assigns) do
